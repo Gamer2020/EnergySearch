eda1f0b02d58de2971c42e9a36757a2332b28c7d
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-13 18:42:13 -0400

    Start to load all cards.

--- cron/es-update-cards.php
+++ cron/es-update-cards.php
@@ -80,7 +80,13 @@ function import_cards()
 
   global $pdo;
 
-  $response = Pokemon::Card()->all();
+  // Get the pagination information for all cards
+  $pagination = Pokemon::Card()->pagination();
+
+  // Get the total number of pages for the card query
+  $total_pages = $pagination->getTotalPages();
+  $page_size = $pagination->getPageSize();
+
 
   $sql = "INSERT INTO es_cards (
               id, name, supertype, subtypes, hp, types, rules, evolves_from, evolves_to,
@@ -161,165 +167,172 @@ function import_cards()
               small_image = :small_image,
               large_image = :large_image,
               ancientTrait = :ancientTrait;";
-  foreach ($response as $model) {
 
-    $cardData = $model->toArray();
+  for ($i = 1; $i <= $total_pages; $i++) {
 
-    $stmt = $pdo->prepare($sql);
+    $response = Pokemon::Card()->page($i)->pageSize($page_size)->all();
+
+    foreach ($response as $model) {
+
+      $cardData = $model->toArray();
+
+      $stmt = $pdo->prepare($sql);
 
 
-    // Bind the values here...
-    $stmt->bindParam(':id', $cardData['id']);
-    $stmt->bindParam(':name', $cardData['name']);
-    $stmt->bindParam(':supertype', $cardData['supertype']);
-    $subtypesvar = arrayToString($cardData['subtypes']);
-    $stmt->bindParam(':subtypes', $subtypesvar);
-    $stmt->bindParam(':hp', $cardData['hp']);
-    $typesvar = arrayToString($cardData['types']);
-    $stmt->bindParam(':types', $typesvar);
-    $rulesvar = arrayToString($cardData['rules']);
-    $stmt->bindParam(':rules', $rulesvar);
-    $stmt->bindParam(':evolves_from', $cardData['evolvesFrom']);
-    $evolvestovar = arrayToString($cardData['evolvesTo']);
-    $stmt->bindParam(':evolves_to', $evolvestovar);
+      // Bind the values here...
+      $stmt->bindParam(':id', $cardData['id']);
+      $stmt->bindParam(':name', $cardData['name']);
+      $stmt->bindParam(':supertype', $cardData['supertype']);
+      $subtypesvar = arrayToString($cardData['subtypes']);
+      $stmt->bindParam(':subtypes', $subtypesvar);
+      $stmt->bindParam(':hp', $cardData['hp']);
+      $typesvar = arrayToString($cardData['types']);
+      $stmt->bindParam(':types', $typesvar);
+      $rulesvar = arrayToString($cardData['rules']);
+      $stmt->bindParam(':rules', $rulesvar);
+      $stmt->bindParam(':evolves_from', $cardData['evolvesFrom']);
+      $evolvestovar = arrayToString($cardData['evolvesTo']);
+      $stmt->bindParam(':evolves_to', $evolvestovar);
 
-    $abilityname1var = '';
-    $abilitytext1var = '';
-    $abilitytype1var = '';
-    $abilityname2var = '';
-    $abilitytext2var = '';
-    $abilitytype2var = '';
+      $abilityname1var = '';
+      $abilitytext1var = '';
+      $abilitytype1var = '';
+      $abilityname2var = '';
+      $abilitytext2var = '';
+      $abilitytype2var = '';
 
-    if (isset($cardData['abilities'][0])) {
-      $ability1 = $cardData['abilities'][0];
-      $abilityname1var = $ability1['name'];
-      $abilitytext1var = $ability1['text'];
-      $abilitytype1var = $ability1['type'];
+      if (isset($cardData['abilities'][0])) {
+        $ability1 = $cardData['abilities'][0];
+        $abilityname1var = $ability1['name'];
+        $abilitytext1var = $ability1['text'];
+        $abilitytype1var = $ability1['type'];
+      }
+
+      if (isset($cardData['abilities'][1])) {
+        $ability2 = $cardData['abilities'][1];
+        $abilityname2var = $ability2['name'];
+        $abilitytext2var = $ability2['text'];
+        $abilitytype2var = $ability2['type'];
+      }
+
+      $stmt->bindParam(':abilityname1', $abilityname1var);
+      $stmt->bindParam(':abilitytext1', $abilitytext1var);
+      $stmt->bindParam(':abilitytype1', $abilitytype1var);
+      $stmt->bindParam(':abilityname2', $abilityname2var);
+      $stmt->bindParam(':abilitytext2', $abilitytext2var);
+      $stmt->bindParam(':abilitytype2', $abilitytype2var);
+
+      $attackname1var = '';
+      $attacktext1var = '';
+      $attackcost1var = '';
+      $attackconvertedEnergyCost1var = '';
+      $attackdamage1var = '';
+
+      $attackname2var = '';
+      $attacktext2var = '';
+      $attackcost2var = '';
+      $attackconvertedEnergyCost2var = '';
+      $attackdamage2var = '';
+
+      $attackname3var = '';
+      $attacktext3var = '';
+      $attackcost3var = '';
+      $attackconvertedEnergyCost3var = '';
+      $attackdamage3var = '';
+
+      $attackname4var = '';
+      $attacktext4var = '';
+      $attackcost4var = '';
+      $attackconvertedEnergyCost4var = '';
+      $attackdamage4var = '';
+
+      if (isset($cardData['attacks'][0])) {
+        $attack1 = $cardData['attacks'][0];
+        $attackname1var = $attack1['name'];
+        $attacktext1var = $attack1['text'];
+        $attackcost1var = json_encode($attack1['cost']);
+        $attackconvertedEnergyCost1var = $attack1['convertedEnergyCost'];
+        $attackdamage1var = $attack1['damage'];
+      }
+
+      if (isset($cardData['attacks'][1])) {
+        $attack2 = $cardData['attacks'][1];
+        $attackname2var = $attack2['name'];
+        $attacktext2var = $attack2['text'];
+        $attackcost2var = json_encode($attack2['cost']);
+        $attackconvertedEnergyCost2var = $attack2['convertedEnergyCost'];
+        $attackdamage2var = $attack2['damage'];
+      }
+
+      if (isset($cardData['attacks'][2])) {
+        $attack3 = $cardData['attacks'][2];
+        $attackname3var = $attack3['name'];
+        $attacktext3var = $attack3['text'];
+        $attackcost3var = json_encode($attack3['cost']);
+        $attackconvertedEnergyCost3var = $attack3['convertedEnergyCost'];
+        $attackdamage3var = $attack3['damage'];
+      }
+
+      if (isset($cardData['attacks'][3])) {
+        $attack4 = $cardData['attacks'][3];
+        $attackname4var = $attack4['name'];
+        $attacktext4var = $attack4['text'];
+        $attackcost4var = json_encode($attack4['cost']);
+        $attackconvertedEnergyCost4var = $attack4['convertedEnergyCost'];
+        $attackdamage4var = $attack4['damage'];
+      }
+
+      $stmt->bindParam(':attackname1', $attackname1var);
+      $stmt->bindParam(':attacktext1', $attacktext1var);
+      $stmt->bindParam(':attackcost1', $attackcost1var);
+      $stmt->bindParam(':attackconvertedenergycost1', $attackconvertedEnergyCost1var);
+      $stmt->bindParam(':attackdamage1', $attackdamage1var);
+
+      $stmt->bindParam(':attackname2', $attackname2var);
+      $stmt->bindParam(':attacktext2', $attacktext2var);
+      $stmt->bindParam(':attackcost2', $attackcost2var);
+      $stmt->bindParam(':attackconvertedenergycost2', $attackconvertedEnergyCost2var);
+      $stmt->bindParam(':attackdamage2', $attackdamage2var);
+
+      $stmt->bindParam(':attackname3', $attackname3var);
+      $stmt->bindParam(':attacktext3', $attacktext3var);
+      $stmt->bindParam(':attackcost3', $attackcost3var);
+      $stmt->bindParam(':attackconvertedenergycost3', $attackconvertedEnergyCost3var);
+      $stmt->bindParam(':attackdamage3', $attackdamage3var);
+
+      $stmt->bindParam(':attackname4', $attackname4var);
+      $stmt->bindParam(':attacktext4', $attacktext4var);
+      $stmt->bindParam(':attackcost4', $attackcost4var);
+      $stmt->bindParam(':attackconvertedenergycost4', $attackconvertedEnergyCost4var);
+      $stmt->bindParam(':attackdamage4', $attackdamage4var);
+
+      $stmt->bindParam(':weaknesstype', $cardData['weaknesses']['type']);
+      $stmt->bindParam(':weaknessvalue', $cardData['weaknesses']['value']);
+      $stmt->bindParam(':resistancetype', $cardData['resistances']['type']);
+      $stmt->bindParam(':resistancevalue', $cardData['resistances']['value']);
+      $retreatcostvar = json_encode($cardData['retreatCost']);
+      $stmt->bindParam(':retreat_cost', $retreatcostvar);
+      $stmt->bindParam(':converted_retreat_cost', $cardData['convertedRetreatCost']);
+      $stmt->bindParam(':set_id', $cardData['set']['id']);
+      $stmt->bindParam(':set_number', $cardData['number']);
+      $stmt->bindParam(':artist', $cardData['artist']);
+      $stmt->bindParam(':rarity', $cardData['rarity']);
+      $stmt->bindParam(':flavor_text', $cardData['flavorText']);
+      $stmt->bindParam(':national_pokedex_numbers', $cardData['nationalPokedexNumbers'][0]);
+      $stmt->bindParam(':unlimited_legality', $cardData['legalities']['unlimited']);
+      $stmt->bindParam(':standard_legality', $cardData['legalities']['standard']);
+      $stmt->bindParam(':expanded_legality', $cardData['legalities']['expanded']);
+      $stmt->bindParam(':small_image', $cardData['images']['small']);
+      $stmt->bindParam(':large_image', $cardData['images']['large']);
+      $stmt->bindParam(':ancientTrait', $cardData['ancientTrait']);
+
+      $defaultviewvar = 0;
+      $stmt->bindParam(':views', $defaultviewvar);
+      $stmt->bindParam(':monthly_views', $defaultviewvar);
+
+      $stmt->execute();
+
     }
-
-    if (isset($cardData['abilities'][1])) {
-      $ability2 = $cardData['abilities'][1];
-      $abilityname2var = $ability2['name'];
-      $abilitytext2var = $ability2['text'];
-      $abilitytype2var = $ability2['type'];
-    }
-
-    $stmt->bindParam(':abilityname1', $abilityname1var);
-    $stmt->bindParam(':abilitytext1', $abilitytext1var);
-    $stmt->bindParam(':abilitytype1', $abilitytype1var);
-    $stmt->bindParam(':abilityname2', $abilityname2var);
-    $stmt->bindParam(':abilitytext2', $abilitytext2var);
-    $stmt->bindParam(':abilitytype2', $abilitytype2var);
-
-    $attackname1var = '';
-    $attacktext1var = '';
-    $attackcost1var = '';
-    $attackconvertedEnergyCost1var = '';
-    $attackdamage1var = '';
-
-    $attackname2var = '';
-    $attacktext2var = '';
-    $attackcost2var = '';
-    $attackconvertedEnergyCost2var = '';
-    $attackdamage2var = '';
-
-    $attackname3var = '';
-    $attacktext3var = '';
-    $attackcost3var = '';
-    $attackconvertedEnergyCost3var = '';
-    $attackdamage3var = '';
-
-    $attackname4var = '';
-    $attacktext4var = '';
-    $attackcost4var = '';
-    $attackconvertedEnergyCost4var = '';
-    $attackdamage4var = '';
-
-    if (isset($cardData['attacks'][0])) {
-      $attack1 = $cardData['attacks'][0];
-      $attackname1var = $attack1['name'];
-      $attacktext1var = $attack1['text'];
-      $attackcost1var = json_encode($attack1['cost']);
-      $attackconvertedEnergyCost1var = $attack1['convertedEnergyCost'];
-      $attackdamage1var = $attack1['damage'];
-    }
-
-    if (isset($cardData['attacks'][1])) {
-      $attack2 = $cardData['attacks'][1];
-      $attackname2var = $attack2['name'];
-      $attacktext2var = $attack2['text'];
-      $attackcost2var = json_encode($attack2['cost']);
-      $attackconvertedEnergyCost2var = $attack2['convertedEnergyCost'];
-      $attackdamage2var = $attack2['damage'];
-    }
-
-    if (isset($cardData['attacks'][2])) {
-      $attack3 = $cardData['attacks'][2];
-      $attackname3var = $attack3['name'];
-      $attacktext3var = $attack3['text'];
-      $attackcost3var = json_encode($attack3['cost']);
-      $attackconvertedEnergyCost3var = $attack3['convertedEnergyCost'];
-      $attackdamage3var = $attack3['damage'];
-    }
-
-    if (isset($cardData['attacks'][3])) {
-      $attack4 = $cardData['attacks'][3];
-      $attackname4var = $attack4['name'];
-      $attacktext4var = $attack4['text'];
-      $attackcost4var = json_encode($attack4['cost']);
-      $attackconvertedEnergyCost4var = $attack4['convertedEnergyCost'];
-      $attackdamage4var = $attack4['damage'];
-    }
-
-    $stmt->bindParam(':attackname1', $attackname1var);
-    $stmt->bindParam(':attacktext1', $attacktext1var);
-    $stmt->bindParam(':attackcost1', $attackcost1var);
-    $stmt->bindParam(':attackconvertedenergycost1', $attackconvertedEnergyCost1var);
-    $stmt->bindParam(':attackdamage1', $attackdamage1var);
-
-    $stmt->bindParam(':attackname2', $attackname2var);
-    $stmt->bindParam(':attacktext2', $attacktext2var);
-    $stmt->bindParam(':attackcost2', $attackcost2var);
-    $stmt->bindParam(':attackconvertedenergycost2', $attackconvertedEnergyCost2var);
-    $stmt->bindParam(':attackdamage2', $attackdamage2var);
-
-    $stmt->bindParam(':attackname3', $attackname3var);
-    $stmt->bindParam(':attacktext3', $attacktext3var);
-    $stmt->bindParam(':attackcost3', $attackcost3var);
-    $stmt->bindParam(':attackconvertedenergycost3', $attackconvertedEnergyCost3var);
-    $stmt->bindParam(':attackdamage3', $attackdamage3var);
-
-    $stmt->bindParam(':attackname4', $attackname4var);
-    $stmt->bindParam(':attacktext4', $attacktext4var);
-    $stmt->bindParam(':attackcost4', $attackcost4var);
-    $stmt->bindParam(':attackconvertedenergycost4', $attackconvertedEnergyCost4var);
-    $stmt->bindParam(':attackdamage4', $attackdamage4var);
-
-    $stmt->bindParam(':weaknesstype', $cardData['weaknesses']['type']);
-    $stmt->bindParam(':weaknessvalue', $cardData['weaknesses']['value']);
-    $stmt->bindParam(':resistancetype', $cardData['resistances']['type']);
-    $stmt->bindParam(':resistancevalue', $cardData['resistances']['value']);
-    $retreatcostvar = json_encode($cardData['retreatCost']);
-    $stmt->bindParam(':retreat_cost', $retreatcostvar);
-    $stmt->bindParam(':converted_retreat_cost', $cardData['convertedRetreatCost']);
-    $stmt->bindParam(':set_id', $cardData['set']['id']);
-    $stmt->bindParam(':set_number', $cardData['number']);
-    $stmt->bindParam(':artist', $cardData['artist']);
-    $stmt->bindParam(':rarity', $cardData['rarity']);
-    $stmt->bindParam(':flavor_text', $cardData['flavorText']);
-    $stmt->bindParam(':national_pokedex_numbers', $cardData['nationalPokedexNumbers'][0]);
-    $stmt->bindParam(':unlimited_legality', $cardData['legalities']['unlimited']);
-    $stmt->bindParam(':standard_legality', $cardData['legalities']['standard']);
-    $stmt->bindParam(':expanded_legality', $cardData['legalities']['expanded']);
-    $stmt->bindParam(':small_image', $cardData['images']['small']);
-    $stmt->bindParam(':large_image', $cardData['images']['large']);
-    $stmt->bindParam(':ancientTrait', $cardData['ancientTrait']);
-
-    $defaultviewvar = 0;
-    $stmt->bindParam(':views', $defaultviewvar);
-    $stmt->bindParam(':monthly_views', $defaultviewvar);
-
-    $stmt->execute();
   }
 }
 
