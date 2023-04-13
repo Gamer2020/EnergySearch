436eee7cf918cf76fc3b5d6fee83879d755e584c
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-13 18:39:12 -0400

    Update setup with messages.

--- cron/es-setup.php
+++ cron/es-setup.php
@@ -1,8 +1,14 @@
 <?php
 require_once('config.php');
 require_once('../config.php');
-
+echo "<h2>Energy Search set up!</h2>";
+echo "The set up is now running!<br>";
+echo "If it takes longer than " . ini_get("max_execution_time") . " seconds the script will error and you may need to edit the value in the config.<br>";
+echo "Installing card sets...";
 require_once('es-update-cardsets.php');
+echo "DONE!<br>";
+echo "Installing cards...";
 require_once('es-update-cards.php');
+echo "DONE!<br>";
 
 ?>
