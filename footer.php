ec7b7288fb68673c277679d2a6504349308612ff
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-15 09:54:37 -0400

    Add text to footer.

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
 <footer>
     <p>&copy;
-        <?php echo date("Y"); ?> My Website. All Rights Reserved.
+        <?php echo date("Y"); ?> Energy Search. All rights reserved.
+        <br>Pokémon characters, images, and card images belong to Pokémon USA, Inc. and Nintendo. This website or it's
+        creators are in no way affiliated with or endorsed by Nintendo LLC, Creatures, GAMEFREAK inc, The Pokémon
+        Company, Pokémon USA, Inc., The Pokémon Company International, or Wizards of the Coast.
+        <br>The use of the copyrighted material on this website falls under <a
+            href="https://web.archive.org/web/20161114112219/http://en.wikipedia.org/wiki/Fair_use">fair use</a>.
+
     </p>
 </footer>
