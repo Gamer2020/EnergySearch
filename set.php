970cde22fb9eb1922c804044c11afaf8575751e4
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-15 12:19:14 -0400

    base set page

---
+++ set.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'include.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "header.php" ?>
+
+<body>
+    <?php include "navbar.php" ?>
+    <div class="container-wide">
+        <div class="panel panel-full">
+
+        
+
+        </div>
+    </div>
+    <?php include "footer.php" ?>
+</body>
+
+</html>
