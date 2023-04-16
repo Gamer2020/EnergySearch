dc09dc12f08fcc809a202e3b0295442b4978ac6d
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-16 11:09:50 -0400

    add more get methods for cards api

--- api/v1/card/index.php
+++ api/v1/card/index.php
@@ -11,7 +11,7 @@ $token = str_replace('Bearer ', '', $authHeader);
 global $pdo;
 
 $stmt = $pdo->prepare("SELECT * FROM es_api_tokens WHERE token_value = ?");
-$stmt->execute(sanitizeInput([$token]));
+$stmt->execute([sanitizeInput($token)]);
 $apiToken = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if (!$apiToken) {
@@ -25,16 +25,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $stmt = $pdo->prepare("SELECT * FROM es_cards WHERE id = ?");
         $stmt->execute([sanitizeInput($_GET['id'])]);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($result) {
-            echo json_encode($result);
-        } else {
-            http_response_code(404);
-            echo json_encode(['error' => 'Card not found']);
-        }
+    } elseif (isset($_GET['set_id']) && isset($_GET['set_number'])) {
+        $stmt = $pdo->prepare("SELECT * FROM es_cards WHERE set_id = ? AND set_number = ?");
+        $stmt->execute([sanitizeInput($_GET['set_id']), sanitizeInput($_GET['set_number'])]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    } elseif (isset($_GET['name'])) {
+        $stmt = $pdo->prepare("SELECT * FROM es_cards WHERE name LIKE ?");
+        $stmt->execute([sanitizeInput('%' . $_GET['name'] . '%')]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     } else {
         http_response_code(400);
         echo json_encode(['error' => 'Invalid request']);
+        exit();
+    }
+
+    if ($result) {
+        echo json_encode($result);
+    } else {
+        http_response_code(404);
+        echo json_encode(['error' => 'Card not found']);
     }
 } else {
     http_response_code(405);
