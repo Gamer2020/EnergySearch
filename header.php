c5d1c227db6427a08dcbbc6f9c70f5b99b0e3fc8
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-18 17:28:38 -0400

    Add favicon to site.

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.png">
     <title>Energy Search | A Pokémon TCG card and deck database!</title>
     <link rel="stylesheet" href="style.css">
 </head>
