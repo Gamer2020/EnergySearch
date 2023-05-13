da9ff4d68222da05aa37b795b99af7f8095b3091
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-13 11:48:09 -0400

    Properly indicate legallity for cards.

--- widgets/sidebar-card-formats.php
+++ widgets/sidebar-card-formats.php
@@ -1,15 +1,54 @@
 <h3>Formats</h3>
-<div class="legality-button-group">
-    <button class="legality-button1" disabled>Standard</button>
-    <button class="legality-button3" disabled>Not Legal</button>
-</div>
 
-<div class="legality-button-group">
-    <button class="legality-button1" disabled>Expanded</button>
-    <button class="legality-button2" disabled>Legal</button>
-</div>
+<?php if (isset($_GET['ID']))
+{
+    if (card_exists(sanitizeInput($_GET['ID'])))
+    {
+        global $pdo;
+        $id = sanitizeInput($_GET['ID']);
+        card_add_view($id);
+        $stmt = $pdo->prepare("SELECT * FROM es_cards WHERE id = ?");
+        $stmt->execute([$id]);
+        $card = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
 
-<div class="legality-button-group">
-    <button class="legality-button1" disabled>Unlimited</button>
-    <button class="legality-button2" disabled>Legal</button>
-</div>
+echo '<div class="legality-button-group">';
+echo '<button class="legality-button1" disabled>Standard</button>';
+if ($card['standard_legality'] == "Legal")
+{
+    echo '<button class="legality-button2" disabled>Legal</button>';
+}
+else
+{
+    echo '<button class="legality-button3" disabled>Not Legal</button>';
+}
+
+echo '</div>';
+
+echo '<div class="legality-button-group">';
+echo '<button class="legality-button1" disabled>Expanded</button>';
+if ($card['expanded_legality'] == "Legal")
+{
+    echo '<button class="legality-button2" disabled>Legal</button>';
+}
+else
+{
+    echo '<button class="legality-button3" disabled>Not Legal</button>';
+}
+echo '</div>';
+
+echo '<div class="legality-button-group">';
+echo '<button class="legality-button1" disabled>Unlimited</button>';
+if ($card['unlimited_legality'] == "Legal")
+{
+    echo '<button class="legality-button2" disabled>Legal</button>';
+}
+else
+{
+    echo '<button class="legality-button3" disabled>Not Legal</button>';
+}
+echo '</div>';
+
+
+?>
