bd382241e45243b1d26e564ed9b7eda106b0f50a
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-13 15:53:25 -0400

    Fix card widget

--- widgets/sidebar-card-formats.php
+++ widgets/sidebar-card-formats.php
@@ -11,44 +11,43 @@
         $stmt->execute([$id]);
         $card = $stmt->fetch(PDO::FETCH_ASSOC);
     }
-}
 
-echo '<div class="legality-button-group">';
-echo '<button class="legality-button1" disabled>Standard</button>';
-if ($card['standard_legality'] == "Legal")
-{
-    echo '<button class="legality-button2" disabled>Legal</button>';
-}
-else
-{
-    echo '<button class="legality-button3" disabled>Not Legal</button>';
-}
+    echo '<div class="legality-button-group">';
+    echo '<button class="legality-button1" disabled>Standard</button>';
+    if ($card['standard_legality'] == "Legal")
+    {
+        echo '<button class="legality-button2" disabled>Legal</button>';
+    }
+    else
+    {
+        echo '<button class="legality-button3" disabled>Not Legal</button>';
+    }
 
-echo '</div>';
+    echo '</div>';
 
-echo '<div class="legality-button-group">';
-echo '<button class="legality-button1" disabled>Expanded</button>';
-if ($card['expanded_legality'] == "Legal")
-{
-    echo '<button class="legality-button2" disabled>Legal</button>';
-}
-else
-{
-    echo '<button class="legality-button3" disabled>Not Legal</button>';
-}
-echo '</div>';
+    echo '<div class="legality-button-group">';
+    echo '<button class="legality-button1" disabled>Expanded</button>';
+    if ($card['expanded_legality'] == "Legal")
+    {
+        echo '<button class="legality-button2" disabled>Legal</button>';
+    }
+    else
+    {
+        echo '<button class="legality-button3" disabled>Not Legal</button>';
+    }
+    echo '</div>';
 
-echo '<div class="legality-button-group">';
-echo '<button class="legality-button1" disabled>Unlimited</button>';
-if ($card['unlimited_legality'] == "Legal")
-{
-    echo '<button class="legality-button2" disabled>Legal</button>';
-}
-else
-{
-    echo '<button class="legality-button3" disabled>Not Legal</button>';
-}
-echo '</div>';
+    echo '<div class="legality-button-group">';
+    echo '<button class="legality-button1" disabled>Unlimited</button>';
+    if ($card['unlimited_legality'] == "Legal")
+    {
+        echo '<button class="legality-button2" disabled>Legal</button>';
+    }
+    else
+    {
+        echo '<button class="legality-button3" disabled>Not Legal</button>';
+    }
+    echo '</div>';
 
-
+}
 ?>
