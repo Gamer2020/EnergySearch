a046b7b912b9bb7d7295a1ba62fe9d75db046620
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-14 00:23:34 -0400

    Only show output if the card exists.

--- widgets/sidebar-card-formats.php
+++ widgets/sidebar-card-formats.php
@@ -10,44 +10,45 @@
         $stmt = $pdo->prepare("SELECT * FROM es_cards WHERE id = ?");
         $stmt->execute([$id]);
         $card = $stmt->fetch(PDO::FETCH_ASSOC);
-    }
 
-    echo '<div class="legality-button-group">';
-    echo '<button class="legality-button1" disabled>Standard</button>';
-    if ($card['standard_legality'] == "Legal")
-    {
-        echo '<button class="legality-button2" disabled>Legal</button>';
-    }
-    else
-    {
-        echo '<button class="legality-button3" disabled>Not Legal</button>';
-    }
+        echo '<div class="legality-button-group">';
+        echo '<button class="legality-button1" disabled>Standard</button>';
+        if ($card['standard_legality'] == "Legal")
+        {
+            echo '<button class="legality-button2" disabled>Legal</button>';
+        }
+        else
+        {
+            echo '<button class="legality-button3" disabled>Not Legal</button>';
+        }
 
-    echo '</div>';
+        echo '</div>';
 
-    echo '<div class="legality-button-group">';
-    echo '<button class="legality-button1" disabled>Expanded</button>';
-    if ($card['expanded_legality'] == "Legal")
-    {
-        echo '<button class="legality-button2" disabled>Legal</button>';
-    }
-    else
-    {
-        echo '<button class="legality-button3" disabled>Not Legal</button>';
-    }
-    echo '</div>';
+        echo '<div class="legality-button-group">';
+        echo '<button class="legality-button1" disabled>Expanded</button>';
+        if ($card['expanded_legality'] == "Legal")
+        {
+            echo '<button class="legality-button2" disabled>Legal</button>';
+        }
+        else
+        {
+            echo '<button class="legality-button3" disabled>Not Legal</button>';
+        }
+        echo '</div>';
 
-    echo '<div class="legality-button-group">';
-    echo '<button class="legality-button1" disabled>Unlimited</button>';
-    if ($card['unlimited_legality'] == "Legal")
-    {
-        echo '<button class="legality-button2" disabled>Legal</button>';
+        echo '<div class="legality-button-group">';
+        echo '<button class="legality-button1" disabled>Unlimited</button>';
+        if ($card['unlimited_legality'] == "Legal")
+        {
+            echo '<button class="legality-button2" disabled>Legal</button>';
+        }
+        else
+        {
+            echo '<button class="legality-button3" disabled>Not Legal</button>';
+        }
+        echo '</div>';
+
     }
-    else
-    {
-        echo '<button class="legality-button3" disabled>Not Legal</button>';
-    }
-    echo '</div>';
 
 }
 ?>
--- widgets/sidebar-deck-formats.php
+++ widgets/sidebar-deck-formats.php
@@ -10,45 +10,45 @@
         $stmt = $pdo->prepare("SELECT * FROM es_decks WHERE id = ?");
         $stmt->execute([$id]);
         $deck = $stmt->fetch(PDO::FETCH_ASSOC);
-    }
 
 
-    echo '<div class="legality-button-group">';
-    echo '<button class="legality-button1" disabled>Standard</button>';
-    if ($deck['standard_legality'] == "Legal")
-    {
-        echo '<button class="legality-button2" disabled>Legal</button>';
-    }
-    else
-    {
-        echo '<button class="legality-button3" disabled>Not Legal</button>';
-    }
 
-    echo '</div>';
+        echo '<div class="legality-button-group">';
+        echo '<button class="legality-button1" disabled>Standard</button>';
+        if ($deck['standard_legality'] == "Legal")
+        {
+            echo '<button class="legality-button2" disabled>Legal</button>';
+        }
+        else
+        {
+            echo '<button class="legality-button3" disabled>Not Legal</button>';
+        }
 
-    echo '<div class="legality-button-group">';
-    echo '<button class="legality-button1" disabled>Expanded</button>';
-    if ($deck['expanded_legality'] == "Legal")
-    {
-        echo '<button class="legality-button2" disabled>Legal</button>';
-    }
-    else
-    {
-        echo '<button class="legality-button3" disabled>Not Legal</button>';
-    }
-    echo '</div>';
+        echo '</div>';
 
-    echo '<div class="legality-button-group">';
-    echo '<button class="legality-button1" disabled>Unlimited</button>';
-    if ($deck['unlimited_legality'] == "Legal")
-    {
-        echo '<button class="legality-button2" disabled>Legal</button>';
-    }
-    else
-    {
-        echo '<button class="legality-button3" disabled>Not Legal</button>';
-    }
-    echo '</div>';
+        echo '<div class="legality-button-group">';
+        echo '<button class="legality-button1" disabled>Expanded</button>';
+        if ($deck['expanded_legality'] == "Legal")
+        {
+            echo '<button class="legality-button2" disabled>Legal</button>';
+        }
+        else
+        {
+            echo '<button class="legality-button3" disabled>Not Legal</button>';
+        }
+        echo '</div>';
 
+        echo '<div class="legality-button-group">';
+        echo '<button class="legality-button1" disabled>Unlimited</button>';
+        if ($deck['unlimited_legality'] == "Legal")
+        {
+            echo '<button class="legality-button2" disabled>Legal</button>';
+        }
+        else
+        {
+            echo '<button class="legality-button3" disabled>Not Legal</button>';
+        }
+        echo '</div>';
+    }
 }
 ?>
