4fec01a94b84dc8feb03c1fc5c5ea4b1ae463295
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-20 21:53:49 -0400

    Add base logic to decks page.

--- decks.php
+++ decks.php
@@ -9,16 +9,41 @@ require_once 'include.php';
 
 <body>
     <?php include "navbar.php" ?>
-    <div class="container">
-        <div class="panel">
+    <?php
+    if ((isset($_GET['search']) && ($_GET['search'] == "search")))
+    {
+        echo '<div class="container-wide">';
+    }
+    else
+    {
+        echo '<div class="container">';
+    }
+    ?>
+    <div class="panel">
 
-            <h1>Decks</h1>
+        <!-- <h1>Decks</h1>
             <p>This is a planned feature that has not been implemented yet...</p>
+            <br> -->
+        <?php //include "widgets/panel-card-search.php" ?>
+        <?php //include "widgets/panel-card-search-results.php" ?>
+        <?php // Stuff that shows if there is no search.
+        if (!(isset($_GET['search']) && ($_GET['search'] == "search")))
+        {
+            //Add stuff here.
+        }
+        ?>
 
-        </div>
-        <aside>
-            <?php include "sidebar/sidebar-generic.php"; ?>
-        </aside>
+    </div>
+    <?php
+    if (!(isset($_GET['search']) && ($_GET['search'] == "search")))
+    {
+
+        echo '<aside>';
+        include "sidebar/sidebar-generic.php";
+        echo '</aside>';
+
+    }
+    ?>
     </div>
     <?php include "footer.php" ?>
 </body>
