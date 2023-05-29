4f6113cd76c39a952871124d91bee4b0b4198424
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-28 23:17:07 -0400

    Give some styling to form elements.

--- widgets/sidebar-card-search.php
+++ widgets/sidebar-card-search.php
@@ -1,5 +1,5 @@
 <h3>Card Search</h3>
 <form action="cards.php">
-    <input type="text" placeholder="Search..." name="cardname">
+    <input type="text" style="width: 135px;" placeholder="Search..." name="cardname">
     <input type="submit" name="search" value="search"></button>
 </form>
