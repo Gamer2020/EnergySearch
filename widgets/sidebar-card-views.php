fd291829f76657fda9adad27638fbdab97c174b0
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-28 22:47:01 -0400

    Fix card view widget formating

--- widgets/sidebar-card-views.php
+++ widgets/sidebar-card-views.php
@@ -2,14 +2,8 @@
 {
     if (card_exists(sanitizeInput($_GET['ID'])))
     {
-        ?>
-        <?php
+
         echo '<h3>Views: ' . get_card_views_by_id(sanitizeInput($_GET['ID'])) . "</h3>";
-        ?>
-        <?php
-
-
-
 
     }
 
